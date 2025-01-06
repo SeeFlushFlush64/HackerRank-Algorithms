c70d583e4284ca99057fe7f2a1ac3702f5407150
Michael Rhey Palaganas <70465004+SeeFlushFlush64@users.noreply.github.com>
2025-01-06 15:51:26 +0800

    added PlusMinus

---
+++ PlusMinus/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        double pos = 0;
+        double neg = 0;
+        double zero = 0;
+        
+        foreach(int num in arr)
+        {
+            if (num > 0) pos++;
+            else if (num < 0) neg++;
+            else zero++;
+        }
+        Console.WriteLine(pos / arr.Count);
+        Console.WriteLine(neg / arr.Count);
+        Console.WriteLine(zero / arr.Count);
+    }
+    
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
